a75a6c0c6c8b910300a45fd2772d0291d96fd074
Fred Alex Martins <fred.abarros@mercadolivre.com>
2022-11-24 12:17:42 -0300

    Blog pessoal Crud modificado em 24112022

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -3,13 +3,13 @@ package com.generation.blogpessoal.controller;
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/postagens")
@@ -22,5 +22,41 @@ public class PostagemController {
     @GetMapping
     public ResponseEntity<List<Postagem>> getAll(){
         return ResponseEntity.ok(postagemRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Postagem> getById(@PathVariable Long id){
+//        Optional<Postagem> buscaPostagem = postagemRepository.findById(id);
+//
+//        if (buscaPostagem.isPresent())
+//            return ResponseEntity.ok(buscaPostagem.get());
+//        else
+//            return ResponseEntity.notFound().build();
+
+        return postagemRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/titulo/{titulo}")
+    public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+        return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+    }
+
+    @PostMapping
+    public ResponseEntity<Postagem> postPostagem(@Valid @RequestBody Postagem postagem) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(postagemRepository.save(postagem));
+    }
+
+    @PutMapping
+    public ResponseEntity<Postagem> putPostagem(@Valid @RequestBody Postagem postagem) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(postagemRepository.save(postagem));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePostagem(@PathVariable Long id) {
+        postagemRepository.deleteById(id);
     }
 }
--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tb_postagens")
@@ -25,7 +25,7 @@ public class Postagem {
     private String texto;
 
     @UpdateTimestamp
-    private LocalDate data;
+    private LocalDateTime data;
 
     public Long getId() {
         return id;
@@ -51,11 +51,11 @@ public class Postagem {
         this.texto = texto;
     }
 
-    public LocalDate getData() {
+    public LocalDateTime getData() {
         return data;
     }
 
-    public void setData(LocalDate data) {
+    public void setData(LocalDateTime data) {
         this.data = data;
     }
 }
