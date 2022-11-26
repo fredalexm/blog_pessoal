6a61986b10608e8e061f34deda2f826006389b21
Fred Alex Martins <fred.abarros@mercadolivre.com>
2022-11-25 21:26:09 -0300

    Blog pessoal Crud modificado em 25112022

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -2,10 +2,13 @@ package com.generation.blogpessoal.controller;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.DelegatingServerHttpResponse;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -19,6 +22,9 @@ public class PostagemController {
     @Autowired
     private PostagemRepository postagemRepository;
 
+    @Autowired
+    private TemaRepository temaRepository;
+
     @GetMapping
     public ResponseEntity<List<Postagem>> getAll(){
         return ResponseEntity.ok(postagemRepository.findAll());
@@ -26,13 +32,6 @@ public class PostagemController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Postagem> getById(@PathVariable Long id){
-//        Optional<Postagem> buscaPostagem = postagemRepository.findById(id);
-//
-//        if (buscaPostagem.isPresent())
-//            return ResponseEntity.ok(buscaPostagem.get());
-//        else
-//            return ResponseEntity.notFound().build();
-
         return postagemRepository.findById(id)
                 .map(resposta -> ResponseEntity.ok(resposta))
                 .orElse(ResponseEntity.notFound().build());
@@ -45,18 +44,36 @@ public class PostagemController {
 
     @PostMapping
     public ResponseEntity<Postagem> postPostagem(@Valid @RequestBody Postagem postagem) {
-        return ResponseEntity.status(HttpStatus.CREATED)
+        if (temaRepository.existsById(postagem.getTema().getId()))
+            return ResponseEntity.status(HttpStatus.CREATED)
                 .body(postagemRepository.save(postagem));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
     @PutMapping
     public ResponseEntity<Postagem> putPostagem(@Valid @RequestBody Postagem postagem) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(postagemRepository.save(postagem));
+
+        if (postagemRepository.existsById(postagem.getId())) {
+
+            if (temaRepository.existsById(postagem.getTema().getId()))
+                return ResponseEntity.status(HttpStatus.OK)
+                        .body(postagemRepository.save(postagem));
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
+
+
     @DeleteMapping("/{id}")
-    public void deletePostagem(@PathVariable Long id) {
-        postagemRepository.deleteById(id);
+    public ResponseEntity<?> deletePostagem(@PathVariable Long id) {
+
+        return postagemRepository.findById(id)
+                .map(resposta -> {
+                    postagemRepository.deleteById(id);
+                    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -1,5 +1,6 @@
 package com.generation.blogpessoal.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
@@ -26,6 +27,11 @@ public class Postagem {
 
     @UpdateTimestamp
     private LocalDateTime data;
+
+    @ManyToOne
+    @JsonIgnoreProperties("postagem")
+    private Tema tema;
+
 
     public Long getId() {
         return id;
@@ -57,5 +63,13 @@ public class Postagem {
 
     public void setData(LocalDateTime data) {
         this.data = data;
+    }
+
+    public Tema getTema() {
+        return tema;
+    }
+
+    public void setTema(Tema tema) {
+        this.tema = tema;
     }
 }
